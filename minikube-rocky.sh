9ab60d10eccd544832ff2aadfb312e0f7e913f67
paulaserna16 <67457991+paulaserna16@users.noreply.github.com>
2026-03-04 13:28:10 +0100

    add kubectl

--- minikube-rocky.sh
+++ minikube-rocky.sh
@@ -17,6 +17,13 @@ systemctl start docker
 echo "Adding $USER_NAME to docker group..."
 usermod -aG docker $USER_NAME
 
+# Install kubectl
+if ! command -v kubectl >/dev/null 2>&1; then
+    curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+    install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+    rm -f kubectl
+fi
+
 echo "Installing Minikube if missing..."
 if ! command -v minikube >/dev/null 2>&1; then
     curl -Lo /usr/local/bin/minikube \
